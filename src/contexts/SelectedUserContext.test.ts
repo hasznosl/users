bb54a20137883660adfab095ca9a7b7d1f10141d
Laszlo Hasznos <hasznos.laszlo@gmail.com>
2019-12-26 11:07:59 +0100

    [TEST]: add selected user context test template

---
+++ src/contexts/SelectedUserContext.test.ts
@@ -0,0 +1,7 @@
+import SelectedUserContext from './SelectedUserContext'
+
+describe('SelectedUserContext', () => {
+  it('should just pass for now', () => {
+    expect(SelectedUserContext).toBeTruthy()
+  })
+})
