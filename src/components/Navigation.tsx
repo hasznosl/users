01cac0e697890f0bdb2d1a2e549b270980a82a6e
Laszlo Hasznos <hasznos.laszlo@gmail.com>
2019-12-28 18:45:28 +0100

    [FEAT]: highlight nav item if active

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,21 +1,42 @@
 import React from 'react'
-import { sticky, NAV_WIDTH, SEARCH_HEIGHT } from '../utils/styles';
+import { sticky, NAV_WIDTH, SEARCH_HEIGHT, blackBorder } from '../utils/styles';
 import { css } from 'glamor';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
-const Navigation = () => <div {...sticky(0)} {...css({
-  height: `calc(100vh - ${SEARCH_HEIGHT}px)`,
-  width: NAV_WIDTH,
-  minWidth: NAV_WIDTH,
-  display: 'flex',
-  alignItems: 'start',
-  flexDirection: 'column',
-  backgroundColor: 'white',
-  borderRight: '1px solid black',
-  padding: `${SEARCH_HEIGHT}px 0px 0px ${SEARCH_HEIGHT}px`
-})}>
-  <Link {...css({ marginBottom: 5 })} to="/">Home</Link>
-  <Link {...css({ marginBottom: 5 })} to="/settings">Settings</Link>
-</div>
+const Navigation = () => {
+
+  const location = useLocation()
+  const highlightIfActive = (pathname: string) => {
+    console.log(location.pathname === pathname)
+    return ((location.pathname === pathname) ? blackBorder(false) : {})
+  }
+
+  return <div {...sticky(0)} {...css({
+    height: `calc(100vh - ${SEARCH_HEIGHT}px)`,
+    width: NAV_WIDTH,
+    minWidth: NAV_WIDTH,
+    display: 'flex',
+    alignItems: 'start',
+    flexDirection: 'column',
+    backgroundColor: 'white',
+    borderRight: '1px solid black',
+    padding: `${SEARCH_HEIGHT}px 0px 0px ${SEARCH_HEIGHT}px`
+  })}>
+    <Link
+      {...css({ padding: 5, marginBottom: 10 })}
+      {...highlightIfActive('/')}
+      to="/"
+    >
+      Home
+    </Link>
+    <Link
+      to="/settings"
+      {...css({ padding: 5 })}
+      {...highlightIfActive('/settings')}
+    >
+      Settings
+    </Link>
+  </div>
+}
 
 export default Navigation
